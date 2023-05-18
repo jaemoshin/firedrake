c742af54accf36f4a17f6372d5f925d4002c11b6
jaemoshin <78436529+jaemoshin@users.noreply.github.com>
2023-05-18 16:04:49 +0100

    Add files via upload

--- auto.py
+++ auto.py
@@ -18,7 +18,7 @@ from data_fit import pcn
 # Define the parameters for pcn
 y_act = result  # Use the result from gauge_set
 c = 0.01
-iterations = 3
+iterations = 100
 beta = 1/2
 cov = np.ones((1, 1))
 nsteps = 1200
