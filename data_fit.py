2f4bb7a2aca65f24e95f796b2ae55ab576351478
jaemoshin <78436529+jaemoshin@users.noreply.github.com>
2023-05-20 13:56:27 +0100

    Add files via upload

--- data_fit.py
+++ data_fit.py
@@ -44,9 +44,12 @@ def pcn(TideSolver, y_act, c = Constant(0.01), iter = 10, beta = 1/2, cov = np.o
 
     unif = np.random.uniform(0,1) 
     
+    c.assign(Constant(np.exp(J)))
+
     y_obs_c = gauge_settwo(TideSolverone, Constant(np.exp(J)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
 
-    TideSolvertwo = TideSolver.solve()
+    c.assign(Constant(np.exp(J_hat)))
+    
     y_obs_c_hat = gauge_settwo(TideSolvertwo, Constant(np.exp(J_hat)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
 
     acc_prob = np.minimum(1, np.exp(phi(y_act, y_obs_c) - phi(y_act, y_obs_c_hat)))
