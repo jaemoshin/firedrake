7b7ed0fa706dc1fb251a927ffb223137c56597fa
jaemoshin <78436529+jaemoshin@users.noreply.github.com>
2023-05-30 09:25:34 +0100

    Add files via upload

--- data_fit.py
+++ data_fit.py
@@ -38,7 +38,7 @@ def pcn(TideSolver, wn, wn1, t, y_act, c = Constant(0.01), iter = 10, beta = 0.0
 
   for k in ProgressBar(f'iterations').iter(range(iter)):
     
-    xi = np.random.multivariate_normal(np.zeros(( len, )) + np.log(0.01), cov , size = len)#Centred Gaussian Measure
+    xi = np.random.multivariate_normal(np.zeros(( len, )), cov , size = len)#Centred Gaussian Measure
     #positive J ~ multivariate normal (log c0, )
     #c = exp(J) 
     #generate both c from the same distribution
@@ -49,11 +49,11 @@ def pcn(TideSolver, wn, wn1, t, y_act, c = Constant(0.01), iter = 10, beta = 0.0
     
     c.assign(Constant(np.exp(J)))
 
-    y_obs_c = gauge_settwo(TideSolver, wn.copy(), wn1, t, Constant(np.exp(J)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
+    y_obs_c = gauge_settwo(TideSolver, wn, wn1, t, Constant(np.exp(J)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
 
     c.assign(Constant(np.exp(J_hat)))
 
-    y_obs_c_hat = gauge_settwo(TideSolver, wn.copy(), wn1, t, Constant(np.exp(J_hat)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
+    y_obs_c_hat = gauge_settwo(TideSolver, wn, wn1, t, Constant(np.exp(J_hat)), t_trunc = 900, gauge_num = 20, nsteps = nsteps)
 
     acc_prob = np.minimum(1, np.exp(phi(y_act, y_obs_c) - phi(y_act, y_obs_c_hat)))
     
