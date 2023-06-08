2a5d992816c20306403b321c0a4aaeb8be00edbd
root <root@DESKTOP-0BGNQ76.localdomain>
2023-06-08 15:34:44 +0100

    D

--- auto.py
+++ auto.py
@@ -14,13 +14,19 @@ TideSolver, wn, wn1, t, F0, c = solve_tides(c = Constant(0.0001))
 # Call the gauge_set function
 result = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
 
+c.assign(0.001)
+
+result2 = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
+
+print(np.linalg.norm(result-result2))
+
 print(result)
 # Import the pcn function from data_fit
 from data_fit import pcn
 
 # Define the parameters for pcn
 
-iterations = 100
+iterations = 1
 beta = 0.025
 cov = np.ones((1, 1))
 
