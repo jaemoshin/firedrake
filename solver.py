e70e6b6c38a547fc874e4c346f4709de25616b20
root <root@DESKTOP-0BGNQ76.localdomain>
2023-06-09 16:57:46 +0100

    d

--- solver.py
+++ solver.py
@@ -61,8 +61,8 @@ def solve_tides(c = Constant(0.0001)):
     Bc = [DirichletBC(W.sub(0), [0,0], "on_boundary")]
     TideProblem = NonlinearVariationalProblem(equation, wn1, bcs = Bc)
     solver_parameters = {
-        'snes_lag_jacobian': 100,
-        'snes_lag_jacobian_persists' : False,
+        'snes_lag_jacobian': -2,
+        #'snes_lag_jacobian_persists' : False,
         'snes_type':'ksponly',
         'mat_type': 'matfree',
         'pc_type': 'python',
