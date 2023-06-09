761eafa10a0b0ae87685d6dce9972d0e5ade4671
root <root@DESKTOP-0BGNQ76.localdomain>
2023-06-09 16:09:31 +0100

    D

--- auto.py
+++ auto.py
@@ -4,30 +4,30 @@ from data_gen import gauge_set
 from data_fit import pcn
 from solver import solve_tides
 from solver import gauge_settwo
-from petsc4py import PETSc
-
 # Define the parameters for gauge_set
 
-t_trunc = 0
+t_trunc = 100
 gauge_num = 20
-nsteps = 100
+nsteps = 200
 TideSolver, wn, wn1, t, F0, c = solve_tides(c = Constant(0.0001))
 
 # Call the gauge_set function
 
 result = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
 
-#c.assign(0.001)
-# Modify the solver parameters to reset the Jacobian computation
-#result2 = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
-#print(np.linalg.norm(result-result2))
-#print(result)
+c.assign(0.001)
+
+result2 = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
+
+print(np.linalg.norm(result-result2))
+
+print(result)
 # Import the pcn function from data_fit
 from data_fit import pcn
 
 # Define the parameters for pcn
 
-iterations = 100
+iterations = 1
 beta = 0.025
 cov = np.ones((1, 1))
 
