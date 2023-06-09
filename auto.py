562f8fb0a8095e86aa22abc1dc9365e756285ffb
root <root@DESKTOP-0BGNQ76.localdomain>
2023-06-09 16:59:57 +0100

    d

--- auto.py
+++ auto.py
@@ -16,6 +16,7 @@ TideSolver, wn, wn1, t, F0, c = solve_tides(c = Constant(0.0001))
 result = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
 print(result)
 c.assign(0.001)
+TideSolver.recompute_jacobian()
 
 result2 = gauge_settwo(TideSolver, wn, wn1, t= t, t_trunc=t_trunc, gauge_num=gauge_num, nsteps=nsteps)
 print(result2)
