5f9106457279e0f38164949872bf76c802c01ba2
root <root@DESKTOP-0BGNQ76.localdomain>
2023-06-09 04:38:08 +0100

    D

--- data_fit.py
+++ data_fit.py
@@ -1,7 +1,7 @@
 import numpy as np
 from firedrake import *
 from solver import gauge_settwo
-
+from solver import solve_tides
 def pcn(TideSolver, wn, wn1, t, y_act, c = Constant(0.001), iter = 10, beta = 0.01, cov = np.ones((1,1)), t_trunc = 900, nsteps = 1200):
 
   import numpy as np
@@ -46,11 +46,11 @@ def pcn(TideSolver, wn, wn1, t, y_act, c = Constant(0.001), iter = 10, beta = 0.
     print(np.exp(J_hat))
     unif = np.random.uniform(0,1) 
     
-    c.assign(Constant(np.exp(J)))
+    #c.assign(Constant(np.exp(J)))
     TideSolver, wn, wn1, t, F0, c = solve_tides(np.exp(J))
     y_obs_c = gauge_settwo(TideSolver, wn, wn1, t, t_trunc = t_trunc, gauge_num = 20, nsteps = nsteps)
 
-    c.assign(Constant(np.exp(J_hat)))
+    #c.assign(Constant(np.exp(J_hat)))
     TideSolver, wn, wn1, t, F0, c = solve_tides(np.exp(J_hat))
     y_obs_c_hat = gauge_settwo(TideSolver, wn, wn1, t, t_trunc = t_trunc, gauge_num = 20, nsteps = nsteps)
     
